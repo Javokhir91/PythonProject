67693105f2969e3cc8baa011eb92b42365302215
Javohir <maska_uz@mail.ru>
2021-10-30 02:03:22 +0300

    Lesson_1_dz

--- lesson_1/lesson_1_dz_1.py
+++ lesson_1/lesson_1_dz_1.py
@@ -16,3 +16,5 @@ elif duration >=3600 and duration < 86400:
 else:
     print(time_day, 'день', time_hour, 'час', time_min, 'мин', time_sec, 'сек')
 
+
+
--- lesson_1/lesson_1_dz_3.py
+++ lesson_1/lesson_1_dz_3.py
@@ -10,3 +10,4 @@ for i in range(100):
         print(i, "процента")
     else:
         print(i, "процентов")
+
